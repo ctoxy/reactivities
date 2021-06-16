d584e1f72516d86fb84131fcf1fcd64d9d8d7750
ctoxy <cedric.bouve@gmail.com>
2021-06-16 14:39:17 +0200

    frontend ok split page and add home

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
+import { store, StoreContext } from './app/stores/store';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />,document.getElementById('root')
+ReactDOM.render(
+    <StoreContext.Provider value={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StoreContext.Provider>,
+    document.getElementById('root')
 );
